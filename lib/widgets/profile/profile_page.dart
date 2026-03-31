5034b2e5c2bd5a9c547340caf6bc5de5bcf51cd5
pedroadm22 <p.plmr45@gmail.com>
2026-03-31 17:02:24 -0300

    Criado o container de boas vindas ao usuario na pagina do perfil do usuario

--- lib/widgets/profile/profile_page.dart
+++ lib/widgets/profile/profile_page.dart
@@ -6,101 +6,101 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.fromLTRB(5, 20, 0, 20),
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromRGBO(50, 50, 93, 0.25),
-                      blurRadius: 100,
-                      spreadRadius: -20,
-                      offset: Offset(0, 50),
-                    ),
-                    BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 0.3),
-                      blurRadius: 60,
-                      spreadRadius: -30,
-                      offset: Offset(0, 30),
-                    ),
-                    BoxShadow(
-                      color: Color.fromRGBO(10, 37, 64, 0.35),
-                      blurRadius: 6,
-                      spreadRadius: 0,
-                      offset: Offset(0, -2),
-                    ),
-                    BoxShadow(
-                      color: Color.fromRGBO(0, 0, 0, 1.0),
-                      blurRadius: 0,
-                      spreadRadius: 0,
-                      offset: Offset(0, 0),
-                    ),
-                  ],
-                  color: Colors.grey[200], // Corrected color shade
-                ),
-                child: Expanded(
-                  child: Column(
-                    children: [
-                      Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Container(
-                                width: 50,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.circular(30)),
-                                child: Icon(Icons.person),
-                              ),
+    final width = MediaQuery.of(context).size.width;
 
-                              RichText(
-                                text: TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: "Bem-vindo de volta, ",
-                                      style: GoogleFonts.montserrat(
-                                        fontSize: 20,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                    TextSpan(
-                                      text: "Pedro Lucas",
-                                      style: GoogleFonts.lato(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              SizedBox(height: 50),
-                              Text(
-                                "Pedro Lucas Martins Rozendo\np.plmr45@gmail.com",
-                                style: GoogleFonts.lato(color: Colors.black),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildHeader(width),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader(double width) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(
+        horizontal: width * 0.04,
+        vertical: width * 0.05,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromRGBO(50, 50, 93, 0.25),
+            blurRadius: 40,
+            spreadRadius: -10,
+            offset: Offset(0, 20),
+          ),
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.2),
+            blurRadius: 20,
+            spreadRadius: -10,
+            offset: Offset(0, 10),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _buildAvatar(width),
+          SizedBox(width: width * 0.04),
+          Expanded(child: _buildTexts(width)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAvatar(double width) {
+    return Container(
+      height: width * 0.2,
+      width: width * 0.2,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: Colors.grey.withAlpha(100),
+      ),
+      child: Icon(
+        Icons.person_rounded,
+        size: width * 0.12,
+      ),
+    );
+  }
+
+  Widget _buildTexts(double width) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text.rich(
+          TextSpan(
+            text: "Bem-vindo de volta, ",
+            style: GoogleFonts.montserrat(
+              fontSize: width * 0.045,
+              color: Colors.black,
+            ),
+            children: [
+              TextSpan(
+                text: "Pedro Lucas!",
+                style: GoogleFonts.ptSans(
+                  fontSize: width * 0.05,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
               ),
             ],
           ),
-        ],
-      ),
+        ),
+        SizedBox(height: width * 0.02),
+        Text(
+          "Aqui você encontra todas as informações relacionadas ao seu perfil",
+          style: GoogleFonts.ptSans(
+            fontSize: width * 0.032,
+            color: Colors.black87,
+          ),
+        ),
+      ],
     );
   }
 }
